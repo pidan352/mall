78a1f426f7295f3d73b7bf32f0070f1476554db8
lyl <1552412358@qq.com>
2023-03-30 09:12:12 +0800

    1.axios未添加的文件 2.修改MBG

--- mall-mapper/src/main/java/com/lyl/GeneratorSqlmap.java
+++ mall-mapper/src/main/java/com/lyl/GeneratorSqlmap.java
@@ -11,20 +11,22 @@ import java.util.List;
 
 public class GeneratorSqlmap {
 
-	public void generator() throws Exception{
+	public void generator() throws Exception {
 
 		List<String> warnings = new ArrayList<>();
 		boolean overwrite = true;
 		//指定 逆向工程配置文件
-		File configFile = new File("generatorConfig.xml"); 
+		File configFile = new File("mall-mapper/src/main/resources/generatorConfig.xml");
 		ConfigurationParser cp = new ConfigurationParser(warnings);
 		Configuration config = cp.parseConfiguration(configFile);
 		DefaultShellCallback callback = new DefaultShellCallback(overwrite);
 		MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config,
-				callback, warnings);
+																 callback, warnings);
 		myBatisGenerator.generate(null);
+		System.out.println(warnings);
 
-	} 
+	}
+
 	public static void main(String[] args) throws Exception {
 		try {
 			GeneratorSqlmap generatorSqlmap = new GeneratorSqlmap();
@@ -32,7 +34,7 @@ public class GeneratorSqlmap {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
+
 	}
 
 }
