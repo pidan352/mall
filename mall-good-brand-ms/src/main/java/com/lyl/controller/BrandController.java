069815837d451910b271bfbd5999fa98d183381f
lyl <1552412358@qq.com>
2023-03-30 10:49:18 +0800

    1.微服务的模块依赖及配置文件2.vue的不知道什么文件

---
+++ mall-good-brand-ms/src/main/java/com/lyl/controller/BrandController.java
@@ -0,0 +1,15 @@
+package com.lyl.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * 功能：品牌管理
+ *
+ * @author 林亦亮
+ * @version 1.0
+ * @date 2023/3/30
+ */
+
+@Controller
+public class BrandController {
+}
